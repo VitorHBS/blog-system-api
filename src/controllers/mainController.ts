732f191091a6f46b9c8bcc313e56a7e439669fc8
Vitor Barbosa <vitorbopelt@gmail.com>
2026-05-17 11:51:51 -0300

    created inicial controllers

---
+++ src/controllers/mainController.ts
@@ -0,0 +1,15 @@
+import { RequestHandler } from "express";
+
+
+
+export const getAllPosts: RequestHandler = async(req, res) => {
+
+}
+
+export const getPosts: RequestHandler = async(req, res) => {
+    
+}
+
+export const getRelatedPost: RequestHandler = async(req, res) => {
+    
+}
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,3 +1,10 @@
 import { Router } from "express";
+import * as adminController from "../controllers/admin";
 
 export const admRouters = Router();
+
+//admRouters.post("/posts", adminController.addPost);
+//admRouters.get("/posts", adminController.getPosts);
+//admRouters.get("/posts/:slug", adminController.getPost);
+//admRouters.put("/posts/:slug", adminController.editPost);
+//admRouters.delete("/posts/:slug", adminController.deletePost);
