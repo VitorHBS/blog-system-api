6e996b12b7bc3f89bd375cdb3ce79a0792d198fd
Vitor Barbosa <vitorbopelt@gmail.com>
2026-05-21 11:45:59 -0300

    created createPostSlug & getPostBySlug

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -2,6 +2,7 @@ import type { RequestHandler, Response } from "express";
 import type { ExtendedRequest } from "../types/extended-request";
 import { z } from "zod";
 import { PostSchema } from "../schemas/post";
+import { createPostSlug, handleCover } from "../services/posts";
 
 export const addPost = async (req: ExtendedRequest, res: Response) => {
     
@@ -9,9 +10,18 @@ export const addPost = async (req: ExtendedRequest, res: Response) => {
 
     const data = PostSchema.safeParse(req.body);
 
-    if(!data.success) return res.status(400).json({error: z.treeifyError(data.error)})
+    if(!data.success) return res.status(400).json({error: z.treeifyError(data.error)});
 
     if(!req.file) return res.status(400).json({error: "Nenhuma arquivo selecionado"});
 
-    
+    //Lidar com o arquivo
+    const coverName = handleCover(req.file);
+    if(!coverName) return res.status(400).json({error: "imagem não permitida/enviada"});
+
+    //Criar o slug do post
+    const slug = await createPostSlug(data.data.title);
+
+    //Criar o Post
+
+    //pegar informações do autor
 }
---
+++ src/services/posts.ts
@@ -0,0 +1,56 @@
+import { v4 } from "uuid";
+import fs from "fs/promises";
+import slug from "slug";
+import { prisma } from "../libs/prisma";
+
+
+export const handleCover = async (file: Express.Multer.File) => {
+    const allowed = ["image/jpeg", "image/png"];
+
+    if (!allowed.includes(file.mimetype)) return false;
+
+    const extension = file.mimetype.split("/")[1];
+    const coverName = `${v4()}.${extension}`;
+
+    try {
+        await fs.rename(
+            file.path,
+            `./public/images/covers/${coverName}`
+        );
+
+        return coverName;
+
+    } catch (err) {
+        return false;
+    }
+}
+
+export const createPostSlug = async (title: string) => {
+    let newSlug = slug(title);
+    let keepTrying = true;
+    let postCount = 1;
+
+    while (keepTrying) {
+        const post = getPostBySlug(newSlug)
+
+        if (!post) {
+            keepTrying = false;
+        } else {
+            newSlug = slug(`${title} ${++postCount}`);
+        }
+    }
+    return newSlug
+}
+
+export const getPostBySlug = async (slug: string) => {
+    return prisma.post.findUnique({
+        where: { slug },
+        include: {
+            author: {
+                select: {
+                    name: true
+                }
+            }
+        }
+    })
+}
